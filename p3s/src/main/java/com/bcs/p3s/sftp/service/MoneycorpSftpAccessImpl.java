a4a8e7ccbde69fabe5380dca8795e615ae982114
andyc <andyc@CCP007.ccp.local>
2017-09-19 14:01:40 +0100

    Provide agreed structure for SFTP-related classes

---
+++ p3s/src/main/java/com/bcs/p3s/sftp/service/MoneycorpSftpAccessImpl.java
@@ -0,0 +1,11 @@
+package com.bcs.p3s.sftp.service;
+
+public class MoneycorpSftpAccessImpl implements MoneycorpSftpAccess {
+
+	
+	
+	
+	
+	
+	
+}
---
+++ p3s/src/main/java/com/bcs/p3s/sftp/service/P3sSftpException.java
@@ -0,0 +1,8 @@
+package com.bcs.p3s.sftp.service;
+
+public class P3sSftpException extends Exception {
+
+    public P3sSftpException(String message) {
+        super(message);
+    }
+}
