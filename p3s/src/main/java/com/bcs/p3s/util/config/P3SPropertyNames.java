e7727eb855a56713fe8a585696e7989f0607d169
andyc <andyc@CCP007.ccp.local>
2017-09-28 11:06:06 +0100

    Update Property file names & property file

--- p3s/src/main/java/com/bcs/p3s/util/config/P3SPropertyNames.java
+++ p3s/src/main/java/com/bcs/p3s/util/config/P3SPropertyNames.java
@@ -1,7 +1,5 @@
 package com.bcs.p3s.util.config;
 
-
-
 /**
  * Convenience class of constants. Avoids typo errors.
  * 
@@ -25,14 +23,25 @@ public interface P3SPropertyNames {
 	int Renewal_Attempts_Constant_In_Payment_Commit = 1;
 	int P3sTransRef_Length = 12;
 	
-	//EPO data
+	// Moneycorp Order file
 	String Destination_EPO_Office = "EPO";
 	String EPO_Payment_Reference = "repl 28051033";
-	
 	String Payment_Type_SEPA = "SEPA";
 	String Payment_Type_SWIFT = "SWIFT";
 
 	//Patent data
 	int Patent_Renewal_Max_Year = 20;
+	
+
+	// SFTP 
+	public static final String WHICH_FX_SFTP_SITE = "fx_provider";
+	public static final String MC_FX_SFTP_SITE_URL = "mc_fx_url";
+	public static final String MC_FX_SFTP_SITE_USERNAME = "mc_fx_un";
+	public static final String MC_FX_SFTP_SITE_SECURE_FINGERPRINT = "mc_fx_key";
+	public static final String BCS_FX_SFTP_SITE_URL = "bcs_fx_url";
+	public static final String BCS_FX_SFTP_SITE_USERNAME = "bcs_fx_un";
+	public static final String BCS_FX_SFTP_SITE_SECURE_FINGERPRINT = "bcs_fx_key";
+
+
 
 }
