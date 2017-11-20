0c5f45df851155b6105e26cd7efa3e9ec7d6fcac
andyc <andyc@CCP007.ccp.local>
2017-11-20 10:44:38 +0000

    Import InvoiceProcessingEngine from cron

--- p3s/src/main/java/com/bcs/p3s/docs/InvoiceProcessingEngine.java
+++ p3s/src/main/java/com/bcs/p3s/docs/InvoiceProcessingEngine.java
@@ -5,9 +5,12 @@ package com.bcs.p3s.docs;
 
 import java.util.Calendar;
 
+import com.bcs.p3s.engine.DummyDataEngine;
 import com.bcs.p3s.enump3s.InvoiceStatusEnum;
 import com.bcs.p3s.enump3s.InvoiceTypeEnum;
 import com.bcs.p3s.model.Invoice;
+import com.bcs.p3s.model.Payment;
+import com.bcs.p3s.util.lang.Universal;
 import com.bcs.p3s.wrap.BankTransferPostCommitDetails;
 
 /**
@@ -18,12 +21,12 @@ import com.bcs.p3s.wrap.BankTransferPostCommitDetails;
  * @author MerinP
  *
  */
-public class InvoiceProcessingEngine {
+public class InvoiceProcessingEngine extends Universal {
 	
 	protected String PREFIX = this.getClass().getName() + " : "; 
 
 	
-	public Invoice populateInvoiceData(BankTransferPostCommitDetails postCommitData){
+	public Invoice populateProformaInvoiceData(BankTransferPostCommitDetails postCommitData){
 		
 		Invoice invoice = new Invoice();
 		
@@ -41,6 +44,7 @@ public class InvoiceProcessingEngine {
 		return invoice;
 	}
 
+
 	public String generateProFormaInvoiceNumber(Invoice invoice) {
 		if (invoice==null) return null;
 		String result = "IP";
@@ -48,5 +52,75 @@ public class InvoiceProcessingEngine {
 		result += "PF";
 		return result;
 	}
+
 	
+	public Invoice populateFinalInvoiceData(Payment payment) {
+		if (payment==null) fail(PREFIX+":populateFinalInvoiceData() passed null");
+		String err = PREFIX+" : populateFinalInvoiceData("+payment.getId()+")";
+
+		Invoice newInvoice = new Invoice(); 
+		Invoice existingInvoice = payment.getLatestInvoice();
+		if (existingInvoice==null) fail(err+" found existingInvoice is missing");
+		
+		newInvoice.setPayment(payment);
+		newInvoice.setInvoiceNumber(stripProForma(existingInvoice.getInvoiceNumber()));
+		newInvoice.setIssueDate(Calendar.getInstance().getTime());
+		newInvoice.setInvoiceStatus(InvoiceStatusEnum.OPEN);
+		newInvoice.setPreceedingInvoiceId(existingInvoice);
+		newInvoice.setInvoiceType(InvoiceTypeEnum.FINAL);
+		//dummy values start
+		newInvoice.setDocPath("hardcodedpdffolder/invoices/");
+		newInvoice.setFilename("dummyInvoiceNumber1.pdf");
+		newInvoice.setInvoiceTemplateId("template1");
+		//dummy values end
+
+		return newInvoice;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public String generateInvoicePDF(Invoice invoice) {
+		if (invoice==null) return null;
+		String err = PREFIX+"generateInvoicePDF("+invoice.getId()+") ";
+		log().debug("invoked "+ err);
+
+		String filename = "dummyInvoiceNumber1.pdf";
+		
+		DummyDataEngine dummy = new DummyDataEngine();  // Flag up incompleteness
+		log().warn("Generating PDFs of Invoices is NOT YET WRITTEN"); // acToDo
+		
+		
+		log().debug("Completed "+ err);
+		return filename;
+	}
+	
+	
+
+	// Support methods
+	
+	/**
+	 * A ProForma invoice number should end 'PF'. 
+	 * The subsequent Final invoice number should be the same number, but without the PF
+	 * This strips the PF 
+	 * @param proFormaInvoiceNumber 
+	 * @return proFormaInvoiceNumber with any trailing PF removed
+	 */
+	protected String stripProForma(String proFormaInvoiceNumber) { // acTidy - relocate this lower
+		if (proFormaInvoiceNumber==null) return proFormaInvoiceNumber;
+		String finalInvoiceNumber = proFormaInvoiceNumber;
+		if (proFormaInvoiceNumber.endsWith("PF")) {
+			finalInvoiceNumber = proFormaInvoiceNumber.substring(0, proFormaInvoiceNumber.length()-2);
+		} 
+		return finalInvoiceNumber;
+	}
+
 }
