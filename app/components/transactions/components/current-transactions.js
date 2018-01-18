1e667e1dc5bfe14aa7a95311799eba158ef849b4
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-01-18 10:42:10 +0000

    Add validation to current-transactions script  - Resolve any issues raised by JSHint

--- app/components/transactions/components/current-transactions.js
+++ app/components/transactions/components/current-transactions.js
@@ -8,15 +8,15 @@ app.component('currentTransactions', {
 
 		var vm = this;
 
-		$rootScope.page = 'Current Transactions'
+		$rootScope.page = 'Current Transactions';
 
 	  	$scope.patentAppData = {
 		  	defaultSelect: null
-	  	}
+	  	};
 
 	  	$scope.clientRefData = {
 		  	defaultSelect: null
-	  	}
+	  	};
 
 		vm.animate = false;
 
@@ -34,18 +34,18 @@ app.component('currentTransactions', {
 		        $scope.clientRefData.defaultSelect = null;
 		    }
 
-		}
+		};
 
 		vm.displayTrans = function() {
 			$state.go('current-transactions');
-		}
+		};
 
 		vm.$onInit = function() {
 
 			var transactions = vm.transactions;
 			transactions.forEach(function(data){
 				data.renewalProgress = currentTransactionsService.renewalProgress(data.latestTransStatus);
-			})
+			});
 
 			vm.tableData = transactions;
 
@@ -57,65 +57,59 @@ app.component('currentTransactions', {
 		   			vm.sortTransDate = true;
 		   			vm.selectedSortType = (function() {
 
-		   				if (vm.sortReverse == false) {
+		   				if (vm.sortReverse === false) {
 		   					vm.tableData.sort(function(a, b){
-		   						console.log(a)
 		   						var dateA = new Date(a.transStartDate), dateB = new Date(b.transStartDate);
 		   						return dateB - dateA;
-		   					})
+		   					});
 		   				} else {
 		   					vm.tableData.sort(function(a, b){
 		   						var dateA = new Date(a.transStartDate), dateB = new Date(b.transStartDate);
-		   						return dateB - dateA
-		   					})
+		   						return dateB - dateA;
+		   					});
 		   				}
-	   				
-	   				}())
+	   				}());
 
 		   		} else if (column == 'transAmount_USD') {
 		   			vm.sortTransCost = true;
 		   			vm.selectedSortType = (function() {
-		   				if (vm.sortReverse == false) {
+		   				if (vm.sortReverse === false) {
 		   					vm.tableData.sort(function(a, b){
 		   						var costA = a.transAmount_USD, costB = b.transAmount_USD;
 		   						return costB - costA;
-		   					})
+		   					});
 		   				} else {
 		   					vm.tableData.sort(function(a, b){
 		   						var costA = a.transAmount_USD, costB = b.transAmount_USD;
 		   						return costB - costA;
-		   					})
+		   					});
 		   				}
-   				
-   					}())
+   					}());
 
 		   		} else if (column == 'transLength') {
 		   			vm.sortTransItems = true;
 		   			vm.selectedSortType = (function() {
-						if (vm.sortReverse == false) {
+						if (vm.sortReverse === false) {
 		   					vm.tableData.sort(function(a, b){
 		   						var renewalsA = a.renewalUIs.length, renewalsB = b.renewalUIs.length;
 		   						return renewalsB - renewalsA;
-		   					})
+		   					});
 		   				} else {
 		   					vm.tableData.sort(function(a, b){
 		   						var renewalsA = a.renewalUIs.length, renewalsB = b.renewalUIs.length;
 		   						return renewalsB - renewalsA;
-		   					})
+		   					});
 		   				}
-	   				}())
+	   				}());
 	   				
 		   		} else {
 		   			vm.sortTransCost = false;
 		   			vm.sortTransDate = false;
-		   			vm.sortTransItems = false
+		   			vm.sortTransItems = false;
 		   			vm.selectedSortType = column;
 		   		}
-		   	}	
-
-
-
-		}
+		   	};
+		};
 
       	vm.rowSelect = function(event){
       		vm.transInfoContent = true;
@@ -124,7 +118,7 @@ app.component('currentTransactions', {
 	      		var patentId = id[0].hash;
 	      		window.location = 'http://localhost:8080/p3sweb/index.htm'+patentId;
       		}
-      	}
+      	};
 	}
 });
 
@@ -132,10 +126,11 @@ app.directive('fixedTableHeaders', ['$timeout', function($timeout) {
   	return {
 	    restrict: 'A',
 	    link: function(scope, element, attrs) {
+        	var container;
 	      	$timeout(function () {
 	      		container = element.parentsUntil(attrs.fixedTableHeaders);
 		        element.stickyTableHeaders({ scrollableArea: container, "fixedOffset": 0 });
 	      	}, 0);
 	    }
-  	}
+  	};
 }]);
