c823fe50fdc2a23d39f4d8497592723f13aa741f
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-01-18 15:49:40 +0000

    Add validation to basketService script  - Resolve any issues raised by JSHint

--- app/components/checkout/services/basketService.js
+++ app/components/checkout/services/basketService.js
@@ -3,6 +3,7 @@ app.factory('basketService', ['$http', '$q', function($http, $q){
 	var factory = {};
 
 		var REST_SERVICE_URI = domain+'rest-basket/';
+		
 		factory.fetchBasketPatents = function(ids) {
 
 			var deferred = $q.defer();
@@ -10,17 +11,17 @@ app.factory('basketService', ['$http', '$q', function($http, $q){
 			$http.post(REST_SERVICE_URI, ids)
 			.then(
 				function(response){
-					deferred.resolve(response.data)
+					deferred.resolve(response.data);
 				},
 				function(errResponse){
-					deferred.reject(errResponse)
+					deferred.reject(errResponse);
 				}
-			)
+			);
 
 			return deferred.promise;
 			
-		}
+		};
 
 	return factory;
 
-}])
+}]);
