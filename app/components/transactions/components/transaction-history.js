7a65befaf840c706266652b0ef174c5268c06319
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-01-18 11:49:09 +0000

    Add validation to transaction-history script  - Resolve any issues raised by JSHint

--- app/components/transactions/components/transaction-history.js
+++ app/components/transactions/components/transaction-history.js
@@ -29,61 +29,58 @@ app.component('transactionHistory', {
 		   			vm.sortTransDate = true;
 		   			vm.selectedSortType = (function() {
 
-		   				if (vm.sortReverse == false) {
+		   				if (vm.sortReverse === false) {
 		   					vm.tableData.sort(function(a, b){
-		   						console.log(a)
 		   						var dateA = new Date(a.transStartDate), dateB = new Date(b.transStartDate);
 		   						return dateB - dateA;
-		   					})
+		   					});
 		   				} else {
 		   					vm.tableData.sort(function(a, b){
 		   						var dateA = new Date(a.transStartDate), dateB = new Date(b.transStartDate);
-		   						return dateB - dateA
-		   					})
+		   						return dateB - dateA;
+		   					});
 		   				}
 	   				
-	   				}())
+	   				}());
 
 		   		} else if (column == 'transAmount_USD') {
 		   			vm.sortTransCost = true;
 		   			vm.selectedSortType = (function() {
-		   				if (vm.sortReverse == false) {
+		   				if (vm.sortReverse === false) {
 		   					vm.tableData.sort(function(a, b){
 		   						var costA = a.transAmount_USD, costB = b.transAmount_USD;
 		   						return costB - costA;
-		   					})
+		   					});
 		   				} else {
 		   					vm.tableData.sort(function(a, b){
 		   						var costA = a.transAmount_USD, costB = b.transAmount_USD;
 		   						return costB - costA;
-		   					})
+		   					});
 		   				}
    				
-   					}())
-
+   					}());
 		   		} else if (column == 'transLength') {
 		   			vm.sortTransItems = true;
 		   			vm.selectedSortType = (function() {
-						if (vm.sortReverse == false) {
+						if (vm.sortReverse === false) {
 		   					vm.tableData.sort(function(a, b){
 		   						var renewalsA = a.renewalUIs.length, renewalsB = b.renewalUIs.length;
 		   						return renewalsB - renewalsA;
-		   					})
+		   					});
 		   				} else {
 		   					vm.tableData.sort(function(a, b){
 		   						var renewalsA = a.renewalUIs.length, renewalsB = b.renewalUIs.length;
 		   						return renewalsB - renewalsA;
-		   					})
+		   					});
 		   				}
-	   				}())
-	   				
+	   				}());
 		   		} else {
 		   			vm.sortTransCost = false;
 		   			vm.sortTransDate = false;
-		   			vm.sortTransItems = false
+		   			vm.sortTransItems = false;
 		   			vm.selectedSortType = column;
 		   		}
-		   	}	
+		   	};
 	   
 	      	vm.rowSelect = function(event){
 	      		vm.transInfoContent = true;
@@ -92,22 +89,20 @@ app.component('transactionHistory', {
 		      		var patentId = id[0].hash;
 		      		window.location = 'http://localhost:8080/p3sweb/index.htm'+patentId;
 	      		}
-	      	}
-		}		
+	      	};
+		};	
 	}
-})
-
+});
 
 app.directive('fixedTableHeaders', ['$timeout', function($timeout) {
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
+      var container;
       $timeout(function () {
-        
-          container = element.parentsUntil(attrs.fixedTableHeaders);
-	        element.stickyTableHeaders({ scrollableArea: container, "fixedOffset": 0 });
-
+      	container = element.parentsUntil(attrs.fixedTableHeaders);
+        element.stickyTableHeaders({ scrollableArea: container, "fixedOffset": 0 });
       }, 0);
     }
-  }
+  };
 }]);
