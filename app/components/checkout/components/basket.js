901086a3cb50ec8d71afeb895971128553638af3
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-01-18 15:47:23 +0000

    Add validation to basket script  - Resolve any issues raised by JSHint

--- app/components/checkout/components/basket.js
+++ app/components/checkout/components/basket.js
@@ -1,14 +1,10 @@
 app.component('basket', {
-	templateUrl: 'p3sweb/app/components/checkout/views/basket.htm',
-	controller: ['basketService', 'ngCart', 'fulfilmentProvider', '$rootScope', '$state',  '$scope', function(basketService, ngCart, fulfilmentProvider, $rootScope, $state, $scope) {
+    templateUrl: 'p3sweb/app/components/checkout/views/basket.htm',
+    controller: ['basketService', 'ngCart', 'fulfilmentProvider', '$rootScope', '$state',  '$scope', function(basketService, ngCart, fulfilmentProvider, $rootScope, $state, $scope) {
 
-		var vm = this;
+        $rootScope.page = 'Basket';
 
-		$rootScope.page = 'Basket';
-
-        console.log($scope.$parent.$parent)
-
-	}
+    }
 ]})
 
 .directive('validateAddress', function(){
@@ -32,6 +28,6 @@ app.component('basket', {
             }
             ctrl.$parsers.push(myValidation);
         }        
-    }    
+    };
 
-})
+});
