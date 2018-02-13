f73c578c833358d719971e80b5b2d9ed43462ce3
MerinP <MerinP@avid-ox790-013.ccp.local>
2018-02-13 12:25:31 +0000

    MP13022018 1224 removing unused classes

--- p3s/src/main/java/com/bcs/p3s/engine/dummyclasses/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * DummyDataEngine was created to TEMPORARILY provide data (maybehardcoded data) until proper data was available
- * 
- * Sometimes, it is approprioate for DummyDataEngine to return a class (a pjoo) - containing several values
- * as these classes are necessarily also temporary, this package is created to hold them
- * to ensure they are all removed in due course 
- *
- */
-package com.bcs.p3s.engine.dummyclasses;
