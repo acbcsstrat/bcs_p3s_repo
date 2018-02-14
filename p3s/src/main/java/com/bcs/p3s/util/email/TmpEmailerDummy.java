1fbf3557ec72ad143e4ce9e9d26f51690453d515
AndyC <AndyC@CCP007.ccp.local>
2018-02-14 16:23:55 +0000

    AC 14022018 1623 clear redundant code

--- p3s/src/main/java/com/bcs/p3s/util/email/TmpEmailerDummy.java
+++ p3s/src/main/java/com/bcs/p3s/util/email/TmpEmailerDummy.java
@@ -11,34 +11,35 @@ import com.bcs.p3s.util.lang.Universal;
  */
 public class TmpEmailerDummy extends Universal {
 
-	public void logEmail(P3sEmail email) {
-		String err = "TmpEmailerDummy logEmail ";
-
-		if (email==null) fail(err +" passed null object");
-		if (email.getSubject()==null) fail(err +" passed null subject");
-		if (email.getHtmlBody()==null) fail(err +" passed null body");
-		
-		
-		System.out.println("\n\n\n");
-		log().info(err+" invoked");
-
-		
-		log().info("Start of Email. Type: "+email.getTemplateName());
-		
-		log().info("Subject:");
-		log().info(email.getSubject());
-
-		log().info("HtmlBody:");
-		log().info(email.getHtmlBody());
-
-//		log().info("Attachment:");
-//		String attach = (email.hasAttachment()) ? email.getAttachmentPath()+"  ::  "+email.getAttachmentFilename(): "None";
-//		log().info(attach);
-
-		log().info("End of Email. Type: "+email.getTemplateName());
-
-		log().info(err+" finished");
-		System.out.println("\n\n\n");
-	}
+// dodo
+//	public void logEmail(P3sEmail email) {
+//		String err = "TmpEmailerDummy logEmail ";
+//
+//		if (email==null) fail(err +" passed null object");
+//		if (email.getSubject()==null) fail(err +" passed null subject");
+//		if (email.getHtmlBody()==null) fail(err +" passed null body");
+//		
+//		
+//		System.out.println("\n\n\n");
+//		log().info(err+" invoked");
+//
+//		
+//		log().info("Start of Email. Type: "+email.getTemplateName());
+//		
+//		log().info("Subject:");
+//		log().info(email.getSubject());
+//
+//		log().info("HtmlBody:");
+//		log().info(email.getHtmlBody());
+//
+////		log().info("Attachment:");
+////		String attach = (email.hasAttachment()) ? email.getAttachmentPath()+"  ::  "+email.getAttachmentFilename(): "None";
+////		log().info(attach);
+//
+//		log().info("End of Email. Type: "+email.getTemplateName());
+//
+//		log().info(err+" finished");
+//		System.out.println("\n\n\n");
+//	}
 	
 }
