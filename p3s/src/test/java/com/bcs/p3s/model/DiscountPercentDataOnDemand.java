d1de6d73cee4656baaf08e48e688b1678538ccc1
MerinP <MerinP@avid-ox790-013.ccp.local>
2018-03-19 15:14:51 +0000

    MP19032018 1513 Deleting integration test classes

--- p3s/src/test/java/com/bcs/p3s/model/DiscountPercentDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.bcs.p3s.model;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = DiscountPercent.class)
-public class DiscountPercentDataOnDemand {
-}
--- p3s/src/test/java/com/bcs/p3s/model/FeeDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.bcs.p3s.model;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Fee.class)
-public class FeeDataOnDemand {
-}
