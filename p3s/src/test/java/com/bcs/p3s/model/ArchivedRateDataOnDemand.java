d80115974b399adf6a7f52bd717407f44a6a6611
MerinP <MerinP@avid-ox790-013.ccp.local>
2018-03-20 08:43:48 +0000

    MP20032018 0843 Removes integration test classes

--- p3s/src/test/java/com/bcs/p3s/model/ArchivedRateDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.bcs.p3s.model;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = ArchivedRate.class)
-public class ArchivedRateDataOnDemand {
-}
--- p3s/src/test/java/com/bcs/p3s/model/P3SFeeSoleDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.bcs.p3s.model;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = P3SFeeSole.class)
-public class P3SFeeSoleDataOnDemand {
-}
