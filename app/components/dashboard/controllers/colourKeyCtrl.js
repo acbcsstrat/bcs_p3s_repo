cfb17aaecc4eb0031467ff84bc98e8b1848bd6b1
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-03-23 12:29:37 +0000

    Add error catch to colourKey  - Add condition to only run script if patent data is available

--- app/components/dashboard/controllers/colourKeyCtrl.js
+++ app/components/dashboard/controllers/colourKeyCtrl.js
@@ -1,15 +1,16 @@
 function colourKeyCtrl($scope, $timeout, patents, patentPhasesService, selectPhaseService) {
 
 	var vm = this;
-	
-	var sortedPatentData = patentPhasesService.phases(patents); //sorts patents into phases and calculate progress
 
 	vm.setPhase = function(phase) {
 		$scope.$emit('phaseChange', {phase: phase})
 		selectPhaseService.setPhase(phase, sortedPatentData); //set current phase being displayed
 	}
 
-	vm.setPhase('green');
+	if(patents) {
+		var sortedPatentData = patentPhasesService.phases(patents); //sorts patents into phases and calculate progress
+		vm.setPhase('green');
+	}
 
 	vm.selectedPhase = selectPhaseService;
 
