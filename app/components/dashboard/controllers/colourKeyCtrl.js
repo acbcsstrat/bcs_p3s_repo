8131edc04095db5e7577e2dc1c5b97b690a407fe
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-03-28 09:08:57 +0100

    Change logic and structure in colourKeyCtrl  - Apply best practices to script

--- app/components/dashboard/controllers/colourKeyCtrl.js
+++ app/components/dashboard/controllers/colourKeyCtrl.js
@@ -2,17 +2,18 @@ function colourKeyCtrl($scope, $timeout, patents, patentPhasesService, selectPha
 
 	var vm = this;
 
-	vm.setPhase = function(phase) {
-		$scope.$emit('phaseChange', {phase: phase})
-		selectPhaseService.setPhase(phase, sortedPatentData); //set current phase being displayed
-	}
+	vm.selectedPhase = selectPhaseService;
+	vm.setPhase = setPhase; 
 
 	if(patents) {
 		var sortedPatentData = patentPhasesService.phases(patents); //sorts patents into phases and calculate progress
 		vm.setPhase('green');
 	}
 
-	vm.selectedPhase = selectPhaseService;
+	function setPhase(phase) {
+		$scope.$emit('phaseChange', {phase: phase})
+		selectPhaseService.setPhase(phase, sortedPatentData); //set current phase being displayed
+	}
 
 }
 
