9b9f11be44fba81aad13909d92eae41a3be92f47
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-03-28 09:15:03 +0100

    Change logic and view in fxChartCtrl  - Apply best practices to fxChart script  - Change view accordingly

--- app/components/dashboard/controllers/fxChartCtrl.js
+++ app/components/dashboard/controllers/fxChartCtrl.js
@@ -2,7 +2,66 @@ function fxChartCtrl($scope, $timeout, patents, patentPhasesService, fxRatesMont
 
 	var vm = this;
 
-	vm.$onInit = function() {
+	$timeout(function() {
+
+		vm.lineData = lineData;
+		vm.lineOptions = {
+			chart: {
+                type: 'lineChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 55,
+                    left: 55
+                },
+                tooltip: {
+				  hideDelay: 0
+				},		                
+                showLegend: false,
+                x: function(d, i){ 
+                	return d[0]},
+                y: function(d){ return d[1]; },
+                useInteractiveGuideline: true,
+                xAxis: {
+		            tickFormat: function (d, i) {
+		                return d3.time.format('%x')(new Date(d));
+		            },
+
+		            showMaxMin: false,
+		            rotateLabels: -30,
+		            ticks: 24        
+                },
+                xScale: d3.time.scale(),
+                yAxis: {
+                    tickFormat: function(d){
+                        return d3.format('.04f')(d);
+                    },
+                    axisLabelDistance: -10,
+                    ticks: 10,
+                    showMaxMin: false
+                },
+                tooltip: {
+                    keyFormatter: function(d) {
+                        return d3.time.format('%x')(new Date(d));
+                    }
+                },		                
+                useVoronoi: false,
+		        lines: {
+		            interactive: true
+		        },
+		        showXAxis: true,
+		        showYAxis: true,
+		        // forceY: [0],	           
+                callback: function(chart){
+
+                }
+            }
+		}
+
+  	}, 200);
+
+ 	function lineData() {
 
 		chartValueArrs = [];
 
@@ -10,89 +69,15 @@ function fxChartCtrl($scope, $timeout, patents, patentPhasesService, fxRatesMont
 			chartValueArrs.push([fxRatesMonth[i].rateActiveDate, fxRatesMonth[i].rate]);
 		}
 
-		if(patents) {
+   		return [
+   			{
+   				values: chartValueArrs.reverse(),
+   				color: '#2ca02c'
+   			}
+   		]
 
-			vm.patentData = patentPhasesService.phases(patents);
+  	} //function end
 
-			$timeout(function() {
-
-				vm.lineOptions = {
-					chart: {
-		                type: 'lineChart',
-		                height: 450,
-		                margin : {
-		                    top: 20,
-		                    right: 20,
-		                    bottom: 55,
-		                    left: 55
-		                },
-		                tooltip: {
-						  hideDelay: 0
-						},		                
-		                showLegend: false,
-		                x: function(d, i){ 
-		                	return d[0]},
-		                y: function(d){ return d[1]; },
-		                useInteractiveGuideline: true,
-		                xAxis: {
-				            tickFormat: function (d, i) {
-				                return d3.time.format('%x')(new Date(d));
-				            },
-
-				            showMaxMin: false,
-				            rotateLabels: -30,
-				            ticks: 24        
-		                },
-		                xScale: d3.time.scale(),
-		                yAxis: {
-		                    tickFormat: function(d){
-		                        return d3.format('.04f')(d);
-		                    },
-		                    axisLabelDistance: -10,
-		                    ticks: 10,
-		                    showMaxMin: false
-		                },
-		                tooltip: {
-		                    keyFormatter: function(d) {
-		                        return d3.time.format('%x')(new Date(d));
-		                    }
-		                },		                
-		                useVoronoi: false,
-				        lines: {
-				            interactive: true
-				        },
-				        showXAxis: true,
-				        showYAxis: true,
-				        // forceY: [0],	           
-		                callback: function(chart){
-
-		                }
-		            }
-				}
-
-	            vm.xAxisTickFormat = function(){
-	                return function(d){
-	                    return d3.time.format('%x')(new Date(d));  //uncomment for date format
-	                }
-	            }
-
-			 	function chartDatafn() {
-
-			   		return [
-			   			{
-			   				values: chartValueArrs.reverse(),
-			   				color: '#2ca02c'
-			   			}
-			   		]
-
-		      	} //function end
-
-		      	vm.lineData = chartDatafn();
-
-	      	}, 200);
-
-		} //if patents end	
-	}
 }
 
 angular.module('ppApp').controller('fxChartCtrl', fxChartCtrl);
