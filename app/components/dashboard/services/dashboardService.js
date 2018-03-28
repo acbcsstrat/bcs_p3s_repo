88e9f9ebf9ff468a0691cd76cabc715122ba7625
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-03-28 11:30:38 +0100

    Change dashboardService to follow best practices  - Change the script in accordance to angular best practices

--- app/components/dashboard/services/dashboardService.js
+++ app/components/dashboard/services/dashboardService.js
@@ -1,55 +1,62 @@
-angular.module('ppApp').factory('dashboardService', ['$http', '$q', function($http, $q){
+angular.module('ppApp').factory('dashboardService', dashboardService);
 
-    var factory = {};
+dashboardService.$inject = ['$http', '$q']
 
-         factory.getMessages = function() {
-            var deferred = $q.defer();
-            $http.get(domain+'login-messages/')
-            .then(
-                function (response) {
-                    deferred.resolve(response.data);
-                },
-                function(errResponse){
-                    console.error('Error while fetching messages');
-                    deferred.reject(errResponse);
-                }
-            );
+function dashboardService($http, $q) {
 
-            return deferred.promise;
-        };
-
-        factory.fetchCostAnalysis = function(id) {
-            
-            var deferred = $q.defer();
-             $http.get(domain+'rest-cost-analysis/'+id)
-                .then(
-                function (response) {
-                    deferred.resolve(response.data);
-                },
-                function(errResponse){
-                    console.error('Error while fetching cost analysis');
-                    deferred.reject(errResponse);
-                }
-            );
-
-            return deferred.promise;
-            
-        };
-
-        factory.supressMessages = function(id) {
-            var deferred = $q.defer();
-            $http.post(domain+'suppress-login-messages/' , id)
-            .then(
-                function(response){
-                    deferred.resolve(response);
-                }, 
-                function(errResponse){
-                    deferred.reject(errResponse);
-                }
-            );
-            return deferred.promise;
-        };
+    var factory = {
+        getMessages: getMessages,
+        fetchCostAnalysis: fetchCostAnalysis
+    };
 
     return factory;
 
-}]);
+    function getMessages() {
+        var deferred = $q.defer();
+        $http.get(domain+'login-messages/')
+        .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while fetching messages');
+                deferred.reject(errResponse);
+            }
+        );
+
+        return deferred.promise;
+    };
+
+    function fetchCostAnalysis(id) {
+        
+        var deferred = $q.defer();
+         $http.get(domain+'rest-cost-analysis/'+id)
+            .then(
+            function (response) {
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while fetching cost analysis');
+                deferred.reject(errResponse);
+            }
+        );
+
+        return deferred.promise;
+        
+    };
+
+        // function supressMessages(id) {
+        //     var deferred = $q.defer();
+        //     $http.post(domain+'suppress-login-messages/' , id)
+        //     .then(
+        //         function(response){
+        //             deferred.resolve(response);
+        //         }, 
+        //         function(errResponse){
+        //             deferred.reject(errResponse);
+        //         }
+        //     );
+        //     return deferred.promise;
+        // };
+
+};
