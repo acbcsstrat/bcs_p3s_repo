43282353eceab5cb4faad3d3530054c0d18477dc
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-03-29 10:49:23 +0100

    Change path url to all files in app.routes  - Correct the path in accordance to the new directory structure

--- app/app.routes.js
+++ app/app.routes.js
@@ -13,7 +13,10 @@ function appRoutes($stateProvider) {
                 }],
                 transactionHistory: ['transactionHistoryService', function(transactionHistoryService) {
                     return transactionHistoryService.fetchTransactionHistory();
-                }],            
+                }],
+                currentTransactions: ['currentTransactionsService', function(currentTransactionsService) {
+                    return currentTransactionsService.fetchCurrentTransactions();
+                }],                       
                 fxRatesWeek: ['fxService', function(fxService) {
                     return fxService.fetchFxWeek();
                 }],
@@ -23,42 +26,42 @@ function appRoutes($stateProvider) {
             },
             views: {
                 '@': {
-                    templateUrl: 'p3sweb/app/components/dashboard/views/dashboard.htm',
+                    templateUrl: 'p3sweb/app/views/dashboard/views/dashboard.htm',
                     controller: 'dashboardCtrl',
                     controllerAs: '$ctrl'
                 },
                 'colourkeywidget@dashboard': {
-                    templateUrl: 'p3sweb/app/components/dashboard/views/ui-views/colour-key-widget.htm',
+                    templateUrl: 'p3sweb/app/views/dashboard/views/ui-views/colour-key-widget.htm',
                     controller: 'colourKeyCtrl',
                     controllerAs: '$ctrl'                
                 },
                 'graphdonutwidget@dashboard': {
                     controller: 'graphDonutCtrl',
                     controllerAs: '$ctrl',                
-                    templateUrl: 'p3sweb/app/components/dashboard/views/ui-views/graph-donut-widget.htm',         
+                    templateUrl: 'p3sweb/app/views/dashboard/views/ui-views/graph-donut-widget.htm',         
                 },
                 'renewalswidget@dashboard': {
-                    templateUrl: 'p3sweb/app/components/dashboard/views/ui-views/renewals-widget.htm',
+                    templateUrl: 'p3sweb/app/views/dashboard/views/ui-views/renewals-widget.htm',
                     controller: 'renewalsCarouselCtrl',
                     controllerAs: '$ctrl'                
                 },
                 'fxrateswidget@dashboard': {
-                    templateUrl: 'p3sweb/app/components/dashboard/views/ui-views/renewal-cost.htm',
+                    templateUrl: 'p3sweb/app/views/dashboard/views/ui-views/renewal-cost.htm',
                     controller: 'renewalCostCtrl',
                     controllerAs: '$ctrl'                
                 },
                 'fxrateswidgetmd@dashboard': {
-                    templateUrl: 'p3sweb/app/components/dashboard/views/ui-views/renewal-cost.htm',
+                    templateUrl: 'p3sweb/app/views/dashboard/views/ui-views/renewal-cost.htm',
                     controller: 'renewalCostCtrl',
                     controllerAs: '$ctrl'                
                 },            
                 'fxchartwidget@dashboard': {
-                    templateUrl: 'p3sweb/app/components/dashboard/views/ui-views/fxchart-widget.htm',
+                    templateUrl: 'p3sweb/app/views/dashboard/views/ui-views/fxchart-widget.htm',
                     controller: 'fxChartCtrl',
                     controllerAs: '$ctrl'
                 },
                 'recentactivitywidget@dashboard': {
-                    templateUrl: 'p3sweb/app/components/dashboard/views/ui-views/recent-activity-widget.htm',
+                    templateUrl: 'p3sweb/app/views/dashboard/views/ui-views/recent-activity-widget.htm',
                     controller: 'recentActivityCtrl',
                     controllerAs: '$ctrl'
                 }            
@@ -66,13 +69,13 @@ function appRoutes($stateProvider) {
         })    
         .state('profile', {
             url: '/profile',
-            templateUrl: 'p3sweb/app/components/user/views/user-profile.htm',
+            templateUrl: 'p3sweb/app/views/user/views/user-profile.htm',
             controller: 'userProfileCtrl',
             controllerAs: '$ctrl'
         })
         .state('patents', {
             url: '/patents',
-            templateUrl: 'p3sweb/app/components/patents/views/list-patents.htm',
+            templateUrl: 'p3sweb/app/views/patents/views/list-patents.htm',
             controller: 'listPatentsCtrl',
             controllerAs: '$ctrl',
             resolve: {
@@ -101,22 +104,22 @@ function appRoutes($stateProvider) {
             },
             views: {
                 '@patents': {
-                    templateUrl: 'p3sweb/app/components/patents/views/patent-item.htm',
+                    templateUrl: 'p3sweb/app/views/patents/views/patent-item.htm',
                     controller: 'patentItemCtrl',
                     controllerAs: '$ctrl'
                 },
                 'patentinfo@patents.patent': {
-                    templateUrl: 'p3sweb/app/components/patents/views/ui-views/patent-info.htm',
+                    templateUrl: 'p3sweb/app/views/patents/views/ui-views/patent-info.htm',
                     controller: 'patentInfoCtrl',
                     controllerAs: '$ctrl'
                 },
                 'patentcostanalysis@patents.patent': {
-                    templateUrl: 'p3sweb/app/components/patents/views/ui-views/patent-costanalysis.htm',
+                    templateUrl: 'p3sweb/app/views/patents/views/ui-views/patent-costanalysis.htm',
                     controller: 'patentCostAnalysisCtrl',
                     controllerAs: '$ctrl'
                 },
                 'patentrenewals@patents.patent': {
-                    templateUrl: 'p3sweb/app/components/patents/views/ui-views/patent-renewals.htm',
+                    templateUrl: 'p3sweb/app/views/patents/views/ui-views/patent-renewals.htm',
                     controller: 'patentRenewalsCtrl',
                     controllerAs: '$ctrl'
                 }
@@ -127,7 +130,7 @@ function appRoutes($stateProvider) {
         })
         .state('search-patent', {
             url: '/search-patent',
-            templateUrl: 'p3sweb/app/components/patents/views/search-patent.htm',
+            templateUrl: 'p3sweb/app/views/patents/views/search-patent.htm',
             controller: 'searchPatentCtrl',
             controllerAs: '$ctrl',
             params: {
@@ -136,7 +139,7 @@ function appRoutes($stateProvider) {
         })
         .state('search-patent.add-patent', {
             url: '?params',
-            templateUrl: 'p3sweb/app/components/patents/views/ui-views/add-patent.htm',
+            templateUrl: 'p3sweb/app/views/patents/views/ui-views/add-patent.htm',
             controller: 'addPatentCtrl',
             controllerAs: '$ctrl',
             params: {
@@ -145,7 +148,7 @@ function appRoutes($stateProvider) {
         })
         .state('current-transactions', {
             url: '/current-transactions',
-            templateUrl: 'p3sweb/app/components/transactions/views/current-transactions.htm',
+            templateUrl: 'p3sweb/app/views/transactions/views/current-transactions.htm',
             controller: 'currentTransactionsCtrl',
             controllerAs: '$ctrl',
             resolve: {
@@ -159,7 +162,7 @@ function appRoutes($stateProvider) {
         })
         .state('current-transactions.current-transaction-item', {
             url: '/{transId}/:transHref',
-            templateUrl: 'p3sweb/app/components/transactions/views/current-transaction-item.htm',
+            templateUrl: 'p3sweb/app/views/transactions/views/current-transaction-item.htm',
             controller: 'currentTransactionItemCtrl',
             controllerAs: '$ctrl',
             resolve: {
@@ -175,7 +178,7 @@ function appRoutes($stateProvider) {
         })
         .state('transaction-history', {
             url: '/transaction-history',
-            templateUrl: 'p3sweb/app/components/transactions/views/transaction-history.htm',
+            templateUrl: 'p3sweb/app/views/transactions/views/transaction-history.htm',
             controller: 'transactionHistoryCtrl',
             controllerAs: '$ctrl',
             resolve: {
@@ -189,7 +192,7 @@ function appRoutes($stateProvider) {
         })
         .state('transaction-history.transaction-history-item', {
             url: '/{transHistoryId}',
-            templateUrl: 'p3sweb/app/components/transactions/views/transaction-history-item.htm',
+            templateUrl: 'p3sweb/app/views/transactions/views/transaction-history-item.htm',
             controller: 'transactionHistoryItemCtrl',
             controllerAs: '$ctrl',            
             resolve: {
@@ -202,13 +205,13 @@ function appRoutes($stateProvider) {
         })
         .state('basket', {
             url: '/basket',
-            templateUrl: 'p3sweb/app/components/checkout/views/basket.htm',
+            templateUrl: 'p3sweb/app/views/checkout/views/basket.htm',
             controller: 'basketCtrl',
             controllerAs: '$ctrl'
         })
         .state('bank-transfer-preparation', {
             url: '/bank-transfer-preparation',
-            templateUrl: 'p3sweb/app/components/checkout/views/bank-transfer-preparation.htm',
+            templateUrl: 'p3sweb/app/views/checkout/views/bank-transfer-preparation.htm',
             controller: 'bankTransferPrepCtrl',
             controllerAs: '$ctrl',
             params: {
@@ -218,7 +221,7 @@ function appRoutes($stateProvider) {
         })    
         .state('bank-transfer-success', {
             url: '/bank-transfer-success',
-            templateUrl: 'p3sweb/app/components/checkout/views/bank-transfer-success.htm',            
+            templateUrl: 'p3sweb/app/views/checkout/views/bank-transfer-success.htm',            
             controller: 'bankTransferSuccessCtrl',
             controllerAs: '$ctrl',            
             params: {
