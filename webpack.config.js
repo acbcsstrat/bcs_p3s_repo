8175f69365a76a0d2f0cd962596cb6b96a232dbc
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-05-03 10:29:21 +0100

    Add configuration to webpack.config  - Add necessary configuration to bundle scss and js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,85 @@
 const path = require('path');
 const ExtractTextPlugin =  require('extract-text-webpack-plugin');
-
-const extractSass = new ExtractTextPlugin({
-    filename: "styles.css",
-    disable: process.env.NODE_ENV === "development"
-});
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-	// context: __dirname + '/app',
-	// entry: {
-	// 	app: [
-	// 		'app.config.js',
-	// 		'app.module.js',
-	// 		'app.routes.js'
-	// 	] 
-	// },
-	// output: {
-	// 	path: __dirname + '/dist',
-	// 	filename: 'app.bundle.js'		
-	// }
-	module: {
-		rules: [{
-			test: /\.scss$/,
-			use: ExtractTextPlugin.extract({
-				use: ['css-loader', 'sass-loader'],
-				fallback: 'style-loader'
-			})
-		}]
+	mode: 'production',
+	context: path.resolve(__dirname, './.'),
+	entry: {
+		app: [
+			'./src/scss/main.scss',
+			'./src/js/index.js'
+		],
+		vendor: './src/js/vendor.js'
 	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+		        include: [
+		          path.resolve(__dirname, "./src/scss")
+		        ],
+		        exclude: [
+		          path.resolve(__dirname, "node_modules")
+		        ],
+				use: ExtractTextPlugin.extract({
+		         	use: [
+			            {
+			                loader: 'css-loader',
+			                options: {
+			                    // If you are having trouble with urls not resolving add this setting.
+			                    // See https://github.com/webpack-contrib/css-loader#url
+			                    url: false,
+			                    minimize: true,
+			                    sourceMap: true
+			                }
+			            }, 
+			            {
+			                loader: 'sass-loader',
+			                options: {
+			                    sourceMap: true
+			                }
+			            }
+		          	],
+					fallback: 'style-loader'
+				})
+			},
+		    {
+		        test: /\.(png|svg|jpg|gif)$/,
+	         	use: [
+	           		'file-loader'
+	         	]
+		    }
+		]
+	},
+   	devServer: {
+     	contentBase: path.join(__dirname, "./dist"),
+     	compress: true,
+     	port: 9000,
+     	hot: true
+   	},	
 	plugins: [
-		extractSass
+		new ExtractTextPlugin({
+			filename: 'main.css'
+		}),
+	  	new UglifyJsPlugin({
+	  		sourceMap: true,
+	    	test: /\.js($|\?)/i
+	  	}),
+	  	new CleanWebpackPlugin(['dist']),
+	  	new HtmlWebpackPlugin({
+	  		title: 'Testing',
+	  		template: 'index.htm'
+
+	  	}),
+     	new webpack.NamedModulesPlugin(),
+     	new webpack.HotModuleReplacementPlugin()	  	
 	]
 }
