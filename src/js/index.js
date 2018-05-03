b3c9b23fb27bbf74ef2448c53133c42557e82777
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-05-03 16:00:12 +0100

    And imports to index.js  - Add local scripts to index.js through import method

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import "localScripts/config/config.js";
+
+import timmyaa from "app/app.module.js";
+import "app/app.config.js";
+import "app/app.routes.js";
+
+function hello() {
+	console.log('hello', timmyaa)
+}
+
+hello()
