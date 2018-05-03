415bc37decdafdef0b2d878c9782f109b14555cd
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-05-03 16:03:59 +0100

    Import all vendor modules to vendor.js

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,3 +1,26 @@
-import 'angular';
-import 'jquery';
+import polyfiller from 'localScripts/js/polyfillers.js';
 
+import 'jquery/dist/jquery.min.js';
+import 'tether/dist/js/tether.min.js';
+
+import 'angular/angular.min.js';
+import 'angular-animate/angular-animate.min.js';
+import 'angular-aria/angular-aria.min.js';
+import 'angular-material/angular-material.min.js';
+import 'angular-ui-bootstrap/dist/ui-bootstrap-tpls.js';
+import 'angular-route/angular-route.min.js';
+import '@uirouter/angularjs/release/angular-ui-router.min.js';
+import 'angular-local-storage/dist/angular-local-storage.min.js';
+
+import 'd3/dist/d3.min.js';
+import 'nvd3/build/nv.d3.min.js';
+import 'angular-nvd3/dist/angular-nvd3.min.js';
+
+import 'moment/min/moment.min.js';
+import 'moment-timezone/builds/moment-timezone.min.js';
+import 'moment-timezone/builds/moment-timezone-with-data-2012-2022.min.js';
+import 'angular-moment/angular-moment.min.js';
+
+import stick from 'localScripts/js/jquery.stickytableheader.js';
+import ngCartDirectives from 'localScripts/js/ngCart.directives.js';
+import ngCartFulfilment from 'localScripts/js/ngCart.fulfilment.js';
