74a464c94b8e9918b7205a6cf35a205e749bac37
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-05-25 11:38:23 +0100

    Fix bug with script syntax  - In testing, an error showed with the variable i, not being assigned  with the var keyword

--- app/services/app.chunkData.serv.js
+++ app/services/app.chunkData.serv.js
@@ -8,7 +8,7 @@ export default function chunkDataService() {
 
 	    	var newArr = [];
 
-    		for (i=0; i < arr.length; i+=size) {
+    		for (var i=0; i < arr.length; i+=size) {
         		newArr.push(arr.slice(i, i+size));
         	}
 
