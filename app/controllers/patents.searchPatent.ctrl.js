9141e3ca6d4af831d8990558c95733e454dc4040
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-06-01 10:08:08 +0100

    Fix bug with error message in searchPatent  - The error message for duplicate patent being entered isnt being  displayed. The page is refreshing. Change reload false to true in  controller

--- app/controllers/patents.searchPatent.ctrl.js
+++ app/controllers/patents.searchPatent.ctrl.js
@@ -12,24 +12,26 @@ function searchPatentCtrl($state, $stateParams, $scope, $rootScope, $timeout, se
       vm.animate = true;
     }, 300);    
 
-    vm.findPatent = findPatent
+    vm.findPatent = findPatent;
 
 	function findPatent(patentNo) {
 		searchPatentService.findPatent(patentNo)
 		.then(
 			function(response) {
-
-				if(response.status == 204 || response.data == '') {
+				console.log(response)
+				if(response.status == 204 && response.data == '') {
 					vm.queriedPatent = null;
-					$state.go('search-patent', {}, {reload: true});
+					$state.go('search-patent', {}, {reload: false});
 					vm.searchError = 'It looks like we’ve already added Patent Application '+patentNo+' in to the system.  You should be able to find it in the List Patents page using the search boxes.';
 				} else {
+					vm.searchError = null;
 					var patentJson = angular.toJson(response)
 					$state.go('search-patent.add-patent', {patent: patentJson});
 				}
 
 			},
 			function(errResponse) {
+				console.log(errResponse)
 				vm.queriedPatent = null;
 				switch(errResponse.status) {
 					case 400:
@@ -40,7 +42,10 @@ function searchPatentCtrl($state, $stateParams, $scope, $rootScope, $timeout, se
 					break;
 					case 204:
 						vm.searchError = 'It looks like we\'ve already added Patent Application '+patentNo+' in to the system.  You should be able to find it in the List Patents page using the search boxes.';
-				}
+					break;
+					default:
+						vm.searchError = null;
+					}
 			}
 		);
     }  	
