42be4f7757c614ddbab5ae492560fd582b36711f
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-08-08 16:23:03 +0100

    Add documents for renewal state  - Add all necessary controllers and templates for the renewal state

---
+++ app/controllers/renewal.renewalCa.ctrl.js
@@ -0,0 +1,41 @@
+angular.module('ppApp').controller('renewalCaCtrl', renewalCaCtrl);
+
+renewalCaCtrl.$inject = ['chunkDataService']
+
+function renewalCaCtrl(chunkDataService) {
+
+    var vm = this;
+
+    vm.displayNotifications = displayNotifications;
+
+    activateData();
+
+    function activateData() {
+        $timeout(function() {
+            vm.displayNotifications('Green');
+        }, 100);
+        if(patent) {
+            fxCalculationService.setFx(patent);
+            vm.patentFx = fxCalculationService;
+        }
+    }
+
+    function displayNotifications(phase) {  //migrate to renewalCtrl
+        vm.chunkedData = chunkDataService.chunkData(phaseNotifications(phase), 8);
+    };
+
+    function phaseNotifications(phase) { //migrate to renewalCtrl
+
+        var notificationsArr = patent.notificationUIs;
+        var notifications = [];
+            
+        notificationsArr.forEach(function(data){
+            if(data.costbandcolor == phase) {
+                notifications.push(data);
+            }
+        });
+
+        return notifications;
+
+    }
+}
