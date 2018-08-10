8756b855013d49649f912c158a8a8b984c6cbdc3
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-08-10 09:20:51 +0100

    Add the view and controller for europct info  - Create the new files with appropriate file names and formatting  - Migrate necessary HTML and script from old controllers and view

--- app/controllers/europct.info.ctrl.js
+++ app/controllers/europct.info.ctrl.js
@@ -1,9 +1,34 @@
 angular.module('ppApp').controller('euroPctInfoCtrl', euroPctInfoCtrl);
 
-euroPctInfoCtrl.$inject = ['$rootScope', '$state', 'searchPatentService', 'patent'];
+euroPctInfoCtrl.$inject = ['$timeout', 'chunkDataService', 'patent'];
 
-function euroPctInfoCtrl($rootScope, $state, searchPatentService, patent) {
+function euroPctInfoCtrl($timeout, chunkDataService, patent) {
 
     var vm = this;
+
+    vm.displayNotifications = displayNotifications;
+
+    $timeout(function() {
+        vm.displayNotifications('Green');
+    }, 100);    
+
+    function displayNotifications(phase) {  //migrate to renewalCtrl
+        vm.chunkedData = chunkDataService.chunkData(phaseNotifications(phase), 8);
+    };
+
+    function phaseNotifications(phase) { //migrate to renewalCtrl
+
+        var notificationsArr = patent.notificationUIs;
+        var notifications = [];
+
+        notificationsArr.forEach(function(data){
+            if(data.costbandcolor == phase) {
+                notifications.push(data);
+            }
+        });
+
+        return notifications;
+
+    }    
 
 }
