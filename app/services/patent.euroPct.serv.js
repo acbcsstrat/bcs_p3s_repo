fae9197a6b5cb930b8fcc56cbeca9a3eca05db3b
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-08-16 10:12:37 +0100

    Add services ready for development  - Add the services for form 1200, eurpopct, and portfolio

---
+++ app/services/patent.euroPct.serv.js
@@ -0,0 +1,17 @@
+angular.module('ppApp').service('euroPhaseEntryCtrlService', euroPhaseEntryCtrlService);
+
+// euroPhaseEntryCtrlCtrl.$inject = [];
+
+function euroPhaseEntryCtrlService() {
+
+    var factory = {
+        fetchPatent: fetchPatent
+    }
+
+    function fetchPatent() {
+        
+    }
+
+    return factory;
+
+}
