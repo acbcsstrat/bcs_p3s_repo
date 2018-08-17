f02cc242d3671a7d8a4d87935d334078234bbfdb
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-08-17 12:28:17 +0100

    Clear cache

--- app/components/patents.patentNav.comp.js
+++
@@ -1,6 +0,0 @@
-angular.module('ppApp').component('patentnav', {
-    template: ['$stateParams', function($stateParams) {
-        var navigation = $stateParams.navigation || 'default';
-        return '<div data-ng-include="\'app/templates/nav.' + navigation + '.tpl.htm\'"></div>'
-    }]
-})
--- assets/js/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-window.ppdomain = "http://localhost:8080/p3sweb/";
-
-
-
-//var domain = "http://demo.thepatent.place:8080/p3sweb/"
