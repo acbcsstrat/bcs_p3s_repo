86f7b83b567e48b4ab96202f53da2e632214744b
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-09-04 09:19:35 +0100

    Add form 1200 generation logic to euroPct  - In the service for euroPct, add the logic to request and return a  promise containing the generated form 1200

--- app/services/patent.euroPct.serv.js
+++ app/services/patent.euroPct.serv.js
@@ -5,9 +5,28 @@ euroPctService.$inject = ['$q', '$http'];
 function euroPctService($q, $http) {
 
     var factory = {
+        generateForm1200: generateForm1200,
         deleteApplication: deleteApplication,
         editApplication: editApplication,
         updateNotifications: updateNotifications
+    }
+
+    function generateForm1200(data) {
+
+        var deferred = $q.defer();
+
+        $http.post(ppdomain+'rest-form1200/'+data)
+        .then(
+            function(response){
+                deferred.resolve(response.data)
+            },
+            function(errResponse){
+                deferred.reject(errResponse.data)
+            }
+        )
+
+        return deferred.promise;
+
     }
 
     function updateNotifications(id) {
