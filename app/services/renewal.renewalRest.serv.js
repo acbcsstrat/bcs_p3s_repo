c46e6dacd5915153ff5f8c570726bd7b94d082d0
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-09-04 09:29:05 +0100

    Add service for notifications in renewals  - Create rest function to return notifications displayed

--- app/services/renewal.renewalRest.serv.js
+++ app/services/renewal.renewalRest.serv.js
@@ -1,31 +1,51 @@
 angular.module('ppApp').service('renewalRestService', renewalRestService);
 
-renewalRestService.$inject = ['$http'];
+renewalRestService.$inject = ['$http', '$q'];
 
-function renewalRestService($http) {
+function renewalRestService($http, $q) {
 
     var factory = {
+        updateNotifications: updateNotifications,
         fetchHistory: fetchHistory
     }
 
     function fetchHistory(id) {
 
         var deferred = $q.defer();
-        
+        console.log(id)
         $http.get(ppdomain+'rest-renewal-history/'+id)
-            .then(
-            function (response) {
+        .then(
+            function(response) {
                 deferred.resolve(response.data);
             },
             function(errResponse){
                 console.error('Error while fetching renewal history');
-                deferred.reject(errResponse);
+                deferred.reject(errResponse.data);
             }
         );
 
         return deferred.promise;
 
     };
+
+    function updateNotifications(id) {
+
+        var deferred = $q.defer();
+
+        $http.get(ppdomain+'rest-renewal-notifications/'+id)
+        .then(
+            function(response){
+                deferred.resolve(response.data);
+            },
+            function(errResponse){
+                console.error('Error while updating notifications');
+                deferred.reject(errResponse.data);
+            }
+        )
+
+        return deferred.promise;
+
+    }
 
     return factory;
 
