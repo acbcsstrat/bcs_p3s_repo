82a055cc095676ca8cc9a868cfe95b400a134bf6
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-10-16 16:19:44 +0100

    Add logic for filtering table based on statuses

--- app/controllers/portfolio.ctrl.js
+++ app/controllers/portfolio.ctrl.js
@@ -1,20 +1,104 @@
 angular.module('ppApp').controller('portfolioCtrl', portfolioCtrl);
 
-portfolioCtrl.$inject = ['patents', '$scope', '$state', '$stateParams','$rootScope', 'patentsRestService', '$timeout', '$uibModal'];
+portfolioCtrl.$inject = ['patents', '$scope', '$state', '$stateParams','$rootScope', 'patentsRestService', '$timeout', '$uibModal', 'chunkDataService', 'filterFilter'];
 
-function portfolioCtrl(patents, $scope, $state, $stateParams, $rootScope, patentsRestService, $timeout, $uibModal) {
+function portfolioCtrl(patents, $scope, $state, $stateParams, $rootScope, patentsRestService, $timeout, $uibModal, chunkDataService, filterFilter) {
 
     var vm = this;
 
     $rootScope.page = 'Portfolio'; 
 
     vm.rowSelect = rowSelect;
-    vm.displayPatents = displayPatents;
+    // vm.displayPatents = displayPatents;
     vm.portfolioData = patents;
+    vm.updateCategory = updateCategory;
+    console.log(vm.portfolioData)
+    vm.filterTopLevelCat = [  
+      {name: 'All Patents'},
+      {name: 'Action Available'},
+      {name: 'No Action Available'}
+    ]    
+
+    var serviceArr = ['All Patents', 'Regional Renewals', 'Euro-PCT'];
+    var europctArr = ['Ready for E-PCT', 'Form 1200 generating', 'Form 1200 saved', 'In Progress'];
+    var renewalArr = ['No Renewal Needed', 'Not Open for Renewal', 'Renewal In Place', 'Open for Renewal', 'Payment in Progress', 'Lapsed'];
+
+    var actionStatus = ['Epct available', 'Epct saved', 'Form 1200 saved', 'In Progress', 'Epct rejected', 'Show price', 'Open for Renewal'];
+
+    vm.filterSecondLevelCat = [
+      {
+        title: 'Services',
+        statuses: (function() {
+          return chunkDataService.chunkData(serviceArr, 4);    
+        }())
+      },
+      {
+        title: 'Euro-PCT',
+        statuses: (function() {
+          return chunkDataService.chunkData(europctArr, 4);    
+        }())
+      },
+      {
+        title: 'Renewal',
+        statuses: (function() {
+          return chunkDataService.chunkData(renewalArr, 4);    
+        }())
+      }
+    ]
+
+    $scope.panelActive = true;
+
+    $scope.selectedItem = {}
+
+    $scope.selectedItem = vm.filterTopLevelCat[0];
+
+    function displayContent(modal) {
+
+      var data = [];
+
+      if(modal.name == 'Action Available') {
+        vm.portfolioData.forEach(function(item){
+          item.serviceList.filter(function(status){
+            console.log(status)
+            if(actionStatus.includes(status.serviceStatus)) {
+              console.log(item)
+              data.push(item)
+            }
+          })
+        })
+      }
+
+      if(modal.name == 'No Action Available') {
+        vm.portfolioData.forEach(function(item){
+          item.serviceList.filter(function(status){
+            if(!actionStatus.includes(status.serviceStatus)) {
+              console.log(item)
+              data.push(item)
+            }
+          })
+        })
+      }      
+      // console.log(data)
+      return data;
+
+    }
+
+    function updateCategory(model) {
+      vm.portfolioData = patents;
+      if(model.name == 'All Patents') {
+
+        vm.portfolioData = patents;
+      } else {
+
+        vm.portfolioData = displayContent(model)
+      }
+
+       
+    }
+
 
     // vm.$onInit = function() {
 
-    //     vm.portfolioData = addItem(patents)
     // }
     
     // function addItem(patents) { //using for testing two services
@@ -35,9 +119,50 @@ function portfolioCtrl(patents, $scope, $state, $stateParams, $rootScope, patent
 
     vm.date = new Date();    
 
-    function displayPatents() { //resets view so only list patents displays
-        $state.go('portfolio');
-    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function displayPatents() { //resets view so only list patents displays
+    //     $state.go('portfolio');
+    // };
 
     function rowSelect(event){
         vm.patentInfoContent = true;
@@ -69,30 +194,24 @@ function portfolioCtrl(patents, $scope, $state, $stateParams, $rootScope, patent
         manualProcessingModal();
     }
 
-      // $scope.oneAtATime = true;
-
-      // $scope.groups = [
-      //   {
-      //     title: 'Dynamic Group Header - 1',
-      //     content: 'Dynamic Group Body - 1'
-      //   },
-      //   {
-      //     title: 'Dynamic Group Header - 2',
-      //     content: 'Dynamic Group Body - 2'
-      //   }
-      // ];
-
-      // $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-      // $scope.addItem = function() {
-      //   var newItemNo = $scope.items.length + 1;
-      //   $scope.items.push('Item ' + newItemNo);
-      // };
-
-      // $scope.status = {
-      //   isCustomHeaderOpen: false,
-      //   isFirstOpen: true,
-      //   isFirstDisabled: false
-      // };
-
 }
+
+
+// .filter('myfilter', function() {
+//    return function( items, condition) {
+//     console.log(items)
+//     var filtered = [];
+
+//     if(condition === undefined || condition === ''){
+//       return items;
+//     }
+
+//     angular.forEach(items, function(item) {          
+//        if(condition === item.condition ||  item.condition === ''){
+//          filtered.push(item);
+//        }
+//     });
+
+//     return filtered;
+//   };
+// });
