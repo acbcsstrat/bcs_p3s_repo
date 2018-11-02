9926cb531a72d39fefb7d90c2954609934b421b3
Patrick McD <patrick.mcdermott@boxcleversoftware.com>
2018-11-02 15:55:02 +0000

    Fix issue with patent item content loading

--- app/controllers/patent.patentItem.ctrl.js
+++ app/controllers/patent.patentItem.ctrl.js
@@ -7,25 +7,27 @@ function patentItemCtrl($rootScope, $scope, $state) {
 	var vm = this;
 
 	vm.activePatentItemMenu = 'Patent Info';
-	vm.loadChart = loadChart;
+	// vm.loadChart = loadChart;
 	vm.loading = true;
 	
 	angular.element(function () {
 		vm.loading = false;
 	    vm.patentLoaded = true;
+	    $state.go('portfolio.patent.patent-info', {}, {reload: false})
 	});	
 
-	vm.$onInit = function() {
-		$state.go('portfolio.patent.patent-info', {}, {reload: false})
-	}
+	// vm.$onInit = function() {
+		
+	// 	console.log('should do')
+	// }
 
-	$scope.$on('renewalHistory', function() {		
-		vm.activePatentItemMenu = 'Renewal History';
-		vm.activeSelectedTab = 2;
-	});	
+	// $scope.$on('renewalHistory', function() {		
+	// 	vm.activePatentItemMenu = 'Renewal History';
+	// 	vm.activeSelectedTab = 2;
+	// });	
 
-	function loadChart() { //required to load chart in costanalysis
-		$rootScope.$broadcast('loadChart')
-	}
+	// function loadChart() { //required to load chart in costanalysis
+	// 	$rootScope.$broadcast('loadChart')
+	// }
 	
 }
