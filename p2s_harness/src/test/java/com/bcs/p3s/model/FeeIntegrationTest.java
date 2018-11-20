c5de918dd33e1af58c362f91f7ed24df1b5d5240
andyc <andyc@CCP007.ccp.local>
2018-11-20 11:11:14 +0000

    181120 1100 Deletions

--- p2s_harness/src/test/java/com/bcs/p3s/model/FeeIntegrationTest.java
+++ p2s_harness/src/test/java/com/bcs/p3s/model/FeeIntegrationTest.java
@@ -2,7 +2,7 @@ package com.bcs.p3s.model;
 import org.junit.Test;
 import org.springframework.roo.addon.test.RooIntegrationTest;
 
-@RooIntegrationTest(entity = Fee.class)
+@RooIntegrationTest(entity = RenewalFee.class)
 public class FeeIntegrationTest {
 
     @Test
