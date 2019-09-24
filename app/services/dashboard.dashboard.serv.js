414fb1efb803d19762c701cf28ae9ee54c045fda
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2019-09-24 12:33:19 +0100

    Fix dashboard bug with colour phases  - Grey is being sent uppercase from backend. Add fucntion to capitlize  all colour phase properties in dashboard service

--- app/services/dashboard.dashboard.serv.js
+++ app/services/dashboard.dashboard.serv.js
@@ -36,20 +36,19 @@ function dashboardService($http, $q, organiseColourService, organiseTextService,
                 } else {
                     obj[property] = 0; //for totoal obj.Total
                 }
-                
             }
         }
 
         obj.Total = patents.length;
 
         for(var i = 0; i < patents.length; i++) {
-
             var item =  patents[i].p3sServices;
-
             if(item.length !== 0) {
-                patents[i].cssCurrent = organiseColourService.getCurrColour(item[0].currentStageColour, 'text')
-                patents[i].cssNext = organiseColourService.getCurrColour(item[0].nextStageColour, 'text')
-                obj[item[0].currentStageColour].push(patents[i]);
+                item.cssCurrent = organiseColourService.getCurrColour(item[0].currentStageColour, 'text')
+                item.cssNext = organiseColourService.getCurrColour(item[0].nextStageColour, 'text')
+                var string = item[0].currentStageColour.toLowerCase();
+                var capitlized = string.charAt(0).toUpperCase() + string.slice(1)
+                obj[capitlized].push(patents[i]);
             } else {    
                 obj.Grey.push(patents[i]);
             }
