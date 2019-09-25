ca73f814d8ae060468d4811bac6c755ef38bfa29
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2019-09-25 15:06:57 +0100

    Add error checks for adding patent  - Add new checks for statuses to display relevant error messages. Wrong  digit, incorrect ep number, already added to system  - Remove redudant code  - Destory timeout on scope destory  - Update patent.id to patent.patentID

--- app/controllers/patents.searchPatent.ctrl.js
+++ app/controllers/patents.searchPatent.ctrl.js
@@ -8,7 +8,7 @@ function searchPatentCtrl($state, $stateParams, $scope, $rootScope, $timeout, se
 
 	vm.pageTitle = 'Search for Patent';
 
-    $timeout(function() {
+    var animateTimeout = $timeout(function() {
       vm.animate = true;
     }, 300);    
 
@@ -19,27 +19,34 @@ function searchPatentCtrl($state, $stateParams, $scope, $rootScope, $timeout, se
 		searchPatentService.findPatent(patentNo)
 		.then(
 			function(response) {
-				if(response.status == 204 && response.data == '') {
-					vm.loadingPatent = false;
-					vm.error = true;
-					$state.go('search-patent', {}, {reload: false});
-					vm.searchError = 'It looks like we\'ve already added Patent Application '+patentNo+' in to the system.  You should be able to find it in the List Patents page using the search boxes.';
-				} else {
-					vm.loadingPatent = false;
-					vm.error = false;
-					var patentJson = angular.toJson(response)
-					$state.go('search-patent.add-patent', {patent: patentJson});
-
-				}
-
+				vm.loadingPatent = false;
+				vm.error = false;
+				var patentJson = angular.toJson(response)
+				$state.go('search-patent.add-patent', {patent: patentJson});
 			},
 			function(errResponse) {
 				vm.queriedPatent = null;
-				vm.searchError = errResponse.data;
+				vm.loadingPatent = false;
+				vm.error = true;
+				$state.go('search-patent', {}, {reload: false});
+				if(response.status == 412) { //already added patent
+					vm.searchError = 'It looks like we\'ve already added Patent Application '+patentNo+' in to the system.  You should be able to find it in the List Patents page using the search boxes.';
+				} 
+				if(response.status == 409){ //incorrect check digit
+					vm.searchError = 'It looks like the provided check digit differs from the check digit found at the European Patent Register. Please make sure the check digit is correct and try again.';
+				}
+				if(response.status == 400) { //incorrect syntax
+					vm.searchError = 'We\'ve not been able to find that patent in the European Patent Register. Please enter an application number such as EP18123456.2';
+				}
+
 
 			}
 		)
 
-    }  	
+    }
+
+    $scope.$on('$destroy', function(){
+    	$timeout.cancel(animateTimeout)
+    })
 
 };
