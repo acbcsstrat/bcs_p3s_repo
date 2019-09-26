1e06bf0c490548e35d7951ea546710171299920b
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2019-09-26 14:01:16 +0100

    Fix error msg bug with add patent  - Correct condition used to check whether an error has occured when  searchin for a patent

--- app/controllers/patents.searchPatent.ctrl.js
+++ app/controllers/patents.searchPatent.ctrl.js
@@ -7,6 +7,7 @@ function searchPatentCtrl($state, $stateParams, $scope, $rootScope, $timeout, se
 	var vm = this;
 
 	vm.pageTitle = 'Search for Patent';
+	vm.queriedPatent = true;
 
     var animateTimeout = $timeout(function() {
       vm.animate = true;
@@ -25,17 +26,18 @@ function searchPatentCtrl($state, $stateParams, $scope, $rootScope, $timeout, se
 				$state.go('search-patent.add-patent', {patent: patentJson});
 			},
 			function(errResponse) {
-				vm.queriedPatent = null;
+
+				vm.queriedPatent = false;
 				vm.loadingPatent = false;
 				vm.error = true;
 				$state.go('search-patent', {}, {reload: false});
-				if(response.status == 412) { //already added patent
+				if(errResponse.status == 412) { //already added patent
 					vm.searchError = 'It looks like we\'ve already added Patent Application '+patentNo+' in to the system.  You should be able to find it in the List Patents page using the search boxes.';
 				} 
-				if(response.status == 409){ //incorrect check digit
+				if(errResponse.status == 409){ //incorrect check digit
 					vm.searchError = 'It looks like the provided check digit differs from the check digit found at the European Patent Register. Please make sure the check digit is correct and try again.';
 				}
-				if(response.status == 400) { //incorrect syntax
+				if(errResponse.status == 400) { //incorrect syntax
 					vm.searchError = 'We\'ve not been able to find that patent in the European Patent Register. Please enter an application number such as EP18123456.2';
 				}
 
