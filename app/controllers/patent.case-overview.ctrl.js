10c1ed742209173e3ae19bcd4cb4995cf99a7f29
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2019-10-21 10:18:28 +0100

    Fix bug with caseoverview object not found

--- app/controllers/patent.case-overview.ctrl.js
+++ app/controllers/patent.case-overview.ctrl.js
@@ -67,7 +67,7 @@ function caseOverviewCtrl(patent, $scope, $state, $stateParams, $timeout, $locat
             if(obj.type == 'Not In Progress') { return; }
 
             if(obj.action == 'epct') {
-                if(ob.status == 'Epct available' || obj.status == 'Epct rejected' || obj.status == 'Await pdf gen start' || obj.status == 'Epct being generated' || obj.status == 'Epct saved' || obj.status == 'EPO Instructed' || obj.status == 'Payment in progress') {
+                if(obj.status == 'Epct available' || obj.status == 'Epct rejected' || obj.status == 'Await pdf gen start' || obj.status == 'Epct being generated' || obj.status == 'Epct saved' || obj.status == 'EPO Instructed' || obj.status == 'Payment in progress') {
                     vm.displayForm1200Tab = true;
                 }
                 
