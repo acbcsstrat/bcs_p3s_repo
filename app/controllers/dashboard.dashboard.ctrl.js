452aae090520d82ec3a612a6131c8eb1ef24cbd7
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2019-11-21 10:16:10 +0000

    Bug with dashboard state change  - Add condition to check for current state of dashboard to change state  to dashboad.content. Prevents users beinf directed back to dashboard if  they change state mid promise

--- app/controllers/dashboard.dashboard.ctrl.js
+++ app/controllers/dashboard.dashboard.ctrl.js
@@ -9,28 +9,24 @@ function dashboardCtrl ($scope, $state, $timeout, dashboardService, patentsRestS
     vm.animate = false;
     vm.pageTitle = 'Dashboard';
     vm.date = new Date().getTime();
-    var dasboardInitTimeout;
+
 
     var promise = patentsRestService.fetchAllPatents();
 
     promise.then(
         function(response){
-            dashboardService.sortPatents(response);
-            $scope.dashboardData = response;
-            $state.go('dashboard.content', {patents: response}, {reload: false});
-            vm.dashboardLoaded = true;
-            
+            if($state.current.name === 'dashboard') {
+                dashboardService.sortPatents(response);
+                $scope.dashboardData = response;
+                $state.go('dashboard.content', {patents: response}, {reload: false});
+                vm.dashboardLoaded = true;
+            }
         }
     )
-
-
 
     $scope.$on('updatePatent', function(e, o){
         $scope.$broadcast('updateCost');
     })    
 
-    $scope.$on('$destroy', function(){
-        $timeout.cancel(dasboardInitTimeout)
-    })
 
 }
