6efd3ee333ad4e65d3e3d4a43fc50ecd96f32822
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2019-11-28 09:05:56 +0000

    Fix digest cycle bug with nav  - timeout function to update time is causing a digest loop to be called  every 500ms. Adjust it so it stops the loop

--- app/components/core.mainNav.comp.js
+++ app/components/core.mainNav.comp.js
@@ -16,11 +16,20 @@ angular.module('ppApp').component('mainnav', {
         	isFirstDisabled: false
       	};
       	vm.avatarimage = '../p3sweb/avatar-image/';
+        vm.utc = moment.tz("Etc/UTC").format('HH:mm MM/DD/YYYY');
+        vm.est = moment.tz("America/New_York").format('HH:mm MM/DD/YYYY');
 
       	$rootScope.$on('refreshAvatar', function(){
 	        var timestamp = new Date().getTime();
 	        vm.avatarimage = '../p3sweb/avatar-image/' + '?' + timestamp;
       	})
+
+		var nbDigest = 0;
+
+		$rootScope.$watch(function() {
+		  	nbDigest++;
+		  	console.log(nbDigest)
+		});
 
       	function init() {
 
@@ -40,10 +49,9 @@ angular.module('ppApp').component('mainnav', {
 	    		}
 	    	)	
 
-		    $timeout(function() {
+		    setInterval(function() {
 		    	timeZoneClocks();
-		    	vm.timeLoaded = true;   
-		    })	    	
+		    }, 1000)
 
       	}
 
@@ -65,16 +73,8 @@ angular.module('ppApp').component('mainnav', {
 	    }
 
 	    function timeZoneClocks() {
-
-	        var utc = moment.tz("Etc/UTC").format('HH:mm MM/DD/YYYY');
-	        var est = moment.tz("America/New_York").format('HH:mm MM/DD/YYYY');
-	    
-	        var t = $timeout(function() {
-	            vm.utcTime = utc;
-	            vm.estTime =  est;
-	            timeZoneClocks()
-	        }, 500);
-
+	        vm.utc = moment.tz("Etc/UTC").format('HH:mm MM/DD/YYYY');
+	        vm.est = moment.tz("America/New_York").format('HH:mm MM/DD/YYYY');
 	    }
 
 		vm.empty = function() {
