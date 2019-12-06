8299edad36ddb43accd0d11168321d3d1b75d50b
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2019-12-06 15:00:19 +0000

    Fix bug with sending orderobj to bank prep  - Correct paramter sent with banktranfer prep post request

--- app/services/vendors/ngCart.fulfilment.js
+++ app/services/vendors/ngCart.fulfilment.js
@@ -25,21 +25,16 @@ angular.module('ngCart.fulfilment', [])
 .service('ngCart.fulfilment.http', ['$http','$state', '$q', function($http, $state, $q){
 
         
-        this.checkout = function(service, patent,order){
-
-            var obj = {
-                totalCostUSD: patent.totalCostUSD,
-                patent_ids: patent.patent_ids
-            };
+        this.checkout = function(service, patent){
 
             var deferred = $q.defer();
-            $http.post('http://localhost:8080/p3sweb/rest-prepare-banktransfer/', obj)
+            $http.post('http://localhost:8080/p3sweb/rest-prepare-banktransfer/', patent)
             .then(
                 function(response){
                     deferred.resolve(response.data);
                 },
                 function(errResponse){
-                    console.log(errResponse)
+                    console.error('Error submitting POST request to rest-prepare-banktransfer/. Error: ', errResponse)
                     deferred.reject(errResponse);
                 }
             );
