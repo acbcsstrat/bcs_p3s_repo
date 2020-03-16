91cd93d05fc067d0857f4a6c8246d3b0d97f45f3
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2020-03-16 13:08:59 +0000

    Add loading icon to fxchart - Add laoding icon to fx chart widget in dashboard whilst fx is being fetched

--- app/controllers/dashboard.fxChart.ctrl.js
+++ app/controllers/dashboard.fxChart.ctrl.js
@@ -7,70 +7,69 @@ function dbfxChartCtrl($scope, fxRatesMonth, $timeout, fxService) {
     var vm = this;
     var fxChartTimeout;
 
-    function init() {
-
-        fxChartTimeout = $timeout(function() {
-
-            vm.lineData = lineData;
-            vm.lineOptions = {
-                chart: {
-                    type: 'lineChart',
-                    height: 450,
-                    margin : {
-                        top: 20,
-                        right: 20,
-                        bottom: 55,
-                        left: 55
-                    },
-                    clipEdge: false,
-                    tooltip: {
-                      hideDelay: 0
-                    },                      
-                    showLegend: false,
-                    x: function(d, i){ 
-                        return d[0]},
-                    y: function(d){ return d[1]; },
-                    useInteractiveGuideline: true,
-                    xAxis: {
-                        tickFormat: function (d, i) {
-                            return d3.time.format('%x')(new Date(d));
+    $scope.$parent.promise
+    .then(
+        function(response){
+            fxChartTimeout = $timeout(function() { 
+                vm.lineData = lineData;
+                vm.lineOptions = {
+                    chart: {
+                        type: 'lineChart',
+                        height: 450,
+                        margin : {
+                            top: 20,
+                            right: 20,
+                            bottom: 55,
+                            left: 55
                         },
+                        clipEdge: false,
+                        tooltip: {
+                          hideDelay: 0
+                        },                      
+                        showLegend: false,
+                        x: function(d, i){ 
+                            return d[0]},
+                        y: function(d){ return d[1]; },
+                        useInteractiveGuideline: true,
+                        xAxis: {
+                            tickFormat: function (d, i) {
+                                return d3.time.format('%x')(new Date(d));
+                            },
 
-                        showMaxMin: false,
-                        rotateLabels: -30,
-                        ticks: 24        
-                    },
-                    xScale: d3.time.scale(),
-                    yAxis: {
-                        tickFormat: function(d){
-                            return d3.format('.04f')(d);
+                            showMaxMin: false,
+                            rotateLabels: -30,
+                            ticks: 24        
                         },
-                        axisLabelDistance: -10,
-                        ticks: 10,
-                        showMaxMin: false
-                    },
-                    tooltip: {
-                        keyFormatter: function(d) {
-                            return d3.time.format('%x')(new Date(d));
+                        xScale: d3.time.scale(),
+                        yAxis: {
+                            tickFormat: function(d){
+                                return d3.format('.04f')(d);
+                            },
+                            axisLabelDistance: -10,
+                            ticks: 10,
+                            showMaxMin: false
+                        },
+                        tooltip: {
+                            keyFormatter: function(d) {
+                                return d3.time.format('%x')(new Date(d));
+                            }
+                        },                      
+                        useVoronoi: false,
+                        lines: {
+                            interactive: true
+                        },
+                        showXAxis: true,
+                        showYAxis: true,
+                        // forceY: [0],            
+                        callback: function(chart){
+
                         }
-                    },                      
-                    useVoronoi: false,
-                    lines: {
-                        interactive: true
-                    },
-                    showXAxis: true,
-                    showYAxis: true,
-                    // forceY: [0],            
-                    callback: function(chart){
-
                     }
                 }
-            }
-
-        }, 2000);
-    }
-
-    init();
+                
+            })
+            vm.fxChartLoaded = true;
+    })
 
     function lineData() {
 
