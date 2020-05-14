37acedce54b7194705cd937aa7050e87403429a4
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2020-05-14 14:40:35 +0100

    add transaction service to fetch all  - Add new service to fetch all transactions. Remove transaction history  request

--- app/features/transactions/services/transactions.service.js
+++ app/features/transactions/services/transactions.service.js
@@ -5,11 +5,12 @@ TransactionService.$inject = ['$http', '$q'];
 function TransactionService($http, $q) {
 
 	var REST_SERVICE_URI_CURRENT = ppdomain+'rest-current-transactions/';
-	var REST_SERVICE_URI_HISTORY = ppdomain+'rest-historic-transactions/';
+	// var REST_SERVICE_URI_HISTORY = ppdomain+'rest-historic-transactions/';
 
 	var factory = {
 		fetchCurrentTransactions:fetchCurrentTransactions,
-		fetchTransactionHistory: fetchTransactionHistory,
+		// fetchTransactionHistory: fetchTransactionHistory,
+		// fetchAllTransactions: fetchAllTransactions
 		fetchAllTransactions: fetchAllTransactions,
 		actionProgress: actionProgress
 	};
@@ -36,10 +37,11 @@ function TransactionService($http, $q) {
 
 		var deferred = $q.defer();
 
-		$q.all([this.fetchCurrentTransactions(), this.fetchTransactionHistory()])
+		$http.get(ppdomain+'rest-all-transactions/')
 		.then(
 			function(response){
-				var concat = response[0].concat(response[1]).map(function(data){	
+				console.log('success response: ', response)
+				response.map(function(data){	
 
 					data.transTypeUI = data.historic === true ? 'Historic' : 'Current';
 	                var isValidation = data.serviceUIs.some(function(item){
@@ -75,12 +77,59 @@ function TransactionService($http, $q) {
 					})
 					return data;		
 				})
-				deferred.resolve(concat);
+				deferred.resolve(response);
 			},
-			function(errResponse) {
+			function(errResponse){
 				console.error('Error fetching all transactions. Error: ', errResponse);
+				deferred.reject(errResponse);
 			}
 		)
+
+		// $q.all([this.fetchCurrentTransactions(), this.fetchTransactionHistory()])
+		// .then(
+		// 	function(response){
+		// 		var concat = response[0].concat(response[1]).map(function(data){	
+
+		// 			data.transTypeUI = data.historic === true ? 'Historic' : 'Current';
+	 //                var isValidation = data.serviceUIs.some(function(item){
+	 //                    return item.newType == 'Validation' ? true : false;
+	 //                })
+
+	 //                if(isValidation === true) {
+	 //                	var valStatus = data.serviceUIs[0].validationStatus;
+	 //                    if((data.latestTransStatus === 'Funds Sent' && valStatus == 'Payment in progress' )|| (valStatus == 'Payment in progress' && data.latestTransStatus === 'EPO Received') || (valStatus == 'PA instructed' && data.latestTransStatus === 'EPO Received') ) {
+	 //                        data.latestTransStatus = 'Processing Funds';
+	 //                    }
+	 //                    if((valStatus !== 'Payment in progress' && data.latestTransStatus === 'EPO Received') || data.latestTransStatus === 'Associates Instructed') {
+	 //                        data.latestTransStatus = 'Processing';
+	 //                    }
+	 //                }
+		// 			data.actionProgress = transactionProgress(isValidation, data.latestTransStatus);
+
+		// 			data.serviceUIs.map(function(o, i){ 
+		// 				if(o.patentUI !== undefined) {				
+		// 					o.appAndType = o.patentUI.ep_ApplicationNumber + ' (' + o.newType +')';	
+		// 					if(o.patentUI.clientRef == '') {
+		// 						o.patentUI.clientRef = '[No Client Reference Provided]'
+		// 					}
+		// 				} else {
+		// 					o.patentUI = {};
+		// 					o.patentUI.clientRef = o.clientRef;
+		// 					o.appAndType = o.patentApplicationNumber + ' (' + o.newType +')';	
+		// 					if(o.patentUI.clientRef == '') {
+		// 						o.patentUI.clientRef = '[No Client Reference Provided]';
+		// 					}
+		// 				}
+		// 				return o;
+		// 			})
+		// 			return data;		
+		// 		})
+		// 		deferred.resolve(concat);
+		// 	},
+		// 	function(errResponse) {
+		// 		console.error('Error fetching all transactions. Error: ', errResponse);
+		// 	}
+		// )
 
 		return deferred.promise;
 
@@ -131,49 +180,49 @@ function TransactionService($http, $q) {
 
 	};
 
-	function fetchTransactionHistory() {
+	// function fetchTransactionHistory() {
 
-		var deferred = $q.defer();
-		$http.get(REST_SERVICE_URI_HISTORY)
-		.then(
-			function(response){
-                response.data.map(function(el){
-                	if(el.renewalUIs.length) {
-	                	el.renewalUIs.map(function(o){ 
-	                		o.newType = 'Renewal';
-	                	})
-                	}
-                	if(el.grantUIs.length) {
-	                	el.grantUIs.map(function(o){ 
-	                		o.newType = 'Grant';
-	                	})
-                	}
-                	if(el.epctUIs.length) {
-	                	el.epctUIs.map(function(o){ 
-	                		o.newType = 'Euro-PCT';
-	                	})
-                	}
-                	if(el.validationUIs.length) {
-	                	el.validationUIs.map(function(o){ 
-	                		o.newType = 'Validation';
-	                	})
-                	}                        	  	
-                    el.serviceUIs = [];
-                    el.serviceUIs = el.serviceUIs.concat(el.renewalUIs, el.grantUIs, el.epctUIs, el.validationUIs)
-                    return el;
+	// 	var deferred = $q.defer();
+	// 	$http.get(REST_SERVICE_URI_HISTORY)
+	// 	.then(
+	// 		function(response){
+ //                response.data.map(function(el){
+ //                	if(el.renewalUIs.length) {
+	//                 	el.renewalUIs.map(function(o){ 
+	//                 		o.newType = 'Renewal';
+	//                 	})
+ //                	}
+ //                	if(el.grantUIs.length) {
+	//                 	el.grantUIs.map(function(o){ 
+	//                 		o.newType = 'Grant';
+	//                 	})
+ //                	}
+ //                	if(el.epctUIs.length) {
+	//                 	el.epctUIs.map(function(o){ 
+	//                 		o.newType = 'Euro-PCT';
+	//                 	})
+ //                	}
+ //                	if(el.validationUIs.length) {
+	//                 	el.validationUIs.map(function(o){ 
+	//                 		o.newType = 'Validation';
+	//                 	})
+ //                	}                        	  	
+ //                    el.serviceUIs = [];
+ //                    el.serviceUIs = el.serviceUIs.concat(el.renewalUIs, el.grantUIs, el.epctUIs, el.validationUIs)
+ //                    return el;
    
-                })
-                deferred.resolve(response.data);
-			},
-			function(errResponse){
-				console.error('Error: unable to fetch transacrtion history: Error msg: ', errResponse)
-				deferred.reject(errResponse);
-			}
-		);
+ //                })
+ //                deferred.resolve(response.data);
+	// 		},
+	// 		function(errResponse){
+	// 			console.error('Error: unable to fetch transacrtion history: Error msg: ', errResponse)
+	// 			deferred.reject(errResponse);
+	// 		}
+	// 	);
 
-		return deferred.promise;
+	// 	return deferred.promise;
 
-	};	
+	// };	
 
 	function actionProgress(currTransStatus) {
 
