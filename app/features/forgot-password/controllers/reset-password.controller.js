813df18e9cadd780530dc75de3ce59e45f98162f
Patrick McDermott <patrick.mcdermott@boxlceversoftware.com>
2020-08-05 17:04:14 +0100

    add zxcvbn method to reset-password  - import zxcvbn libarary to reset password and set up function to  return password strength  - amend html to assign values to scope

--- app/features/forgot-password/controllers/reset-password.controller.js
+++ app/features/forgot-password/controllers/reset-password.controller.js
@@ -1,3 +1,5 @@
+import zxcvbn from 'zxcvbn';
+
 ResestPasswordController.$inject = ['$state', '$rootScope','$http', '$scope', '$cookies', 'AuthorisationService']
 
 export default function ResestPasswordController($state, $rootScope, $http, $scope, $cookies, AuthorisationService) {
@@ -5,6 +7,29 @@ export default function ResestPasswordController($state, $rootScope, $http, $sco
 	var vm = this;
 
 	vm.submitEmail = submitEmail;
+	vm.formData = {};
+
+    vm.passwordUpdate = passwordUpdate
+  
+    function passwordUpdate(password) {
+
+        if(password !== undefined) {
+            if(vm.formData.password.length < 8){ //https://stackoverflow.com/questions/56314220/angularjs-minlength-validation-stop-characters-counter
+                $scope.resetPasswordForm.password.$setValidity('minlength', false);
+            } else{
+                $scope.resetPasswordForm.password.$setValidity('minlength', true);
+            }
+
+            if(vm.formData.password.length > 20){
+                $scope.resetPasswordForm.password.$setValidity('maxlength', false);
+            } else{
+                $scope.resetPasswordForm.password.$setValidity('maxlength', true);
+            }
+
+            vm.passwordStrength = zxcvbn(password);
+        }
+        
+    }	
 
 	function submitEmail(password) {
 
